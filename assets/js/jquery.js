68c4de3cb75b356d4a2959850ed5a3967817be5b
ay-ks <atsumi_yamashita@kaisei-group.co.jp>
2020-07-02 17:58:42 +0900

    commit

--- assets/js/jquery.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-	var windowH = $(window).height(),
-		scrollY = $(window).scrollTop(),
-		elPosition = $('.bg_anime').offset().top;
-	if (scrollY > elPosition - windowH) {
-		$('.bg_anime').addClass("start_anime");
-	}
-});
